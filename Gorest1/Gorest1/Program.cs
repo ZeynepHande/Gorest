30c227006b4df5e4cc7315fe3f70de42a5c5cf53
ZeynepHande <zeynephandega@gmail.com>
2022-11-06 17:37:57 +0300

    Project 4

---
+++ Gorest1/Gorest1/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json;
+using static System.Net.Http.HttpClient;
+
+namespace Gorest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // string connection = "https://gorest.co.in/public/v2/users";
+            string baglanti = "https://gorest.co.in/public/v2/users";
+
+            var data = HttpHelper.GetDataFromApi<List<Urun>>(baglanti).Result;
+           // Console.Read();
+            WebRequest istek = HttpWebRequest.Create(baglanti);
+            WebResponse cevap;
+
+            cevap = istek.GetResponse();
+
+            StreamReader donenBilgi = new StreamReader(cevap.GetResponseStream());
+
+            string bilgiAl = donenBilgi.ReadToEnd().Trim();
+
+            List<Urun> urunBilgisi = JsonConvert.DeserializeObject<List<Urun>>(bilgiAl);
+            // List<Urun> urunBilgisi = Newtonsoft.Json.JsonConvert.DeserializeObject<Urun>(bilgiAl);
+            ////JsonConvert.DeserializeObject
+            // //XDocument gorest = XDocument.Load(connection);
+
+            //var id = urunBilgisi.Descendants("id").ElementAt(0);
+            // for (int i = 0; i < items[0].)
+           // var items = JsonConvert.DeserializeObject<List<Urun>>(bilgiAl);
+            Console.WriteLine("List1:");
+            foreach (var item in urunBilgisi)
+            {
+                
+                if (item.status == "active")
+                {
+                    Console.WriteLine(item.id);
+                }
+            }
+            Console.WriteLine("List2:");
+            foreach (var item in urunBilgisi)
+            {
+
+                if (item.status == "inactive")
+                {
+                    Console.WriteLine(item.id);
+                }
+            }
+
+            //Console.WriteLine(bilgiAl.ToString());
+
+            Console.WriteLine("miktar" + ":" + data.Count.ToString());
+            Console.ReadLine();
+
+            //List<Urun> urun = bilgiAl.Select(c => new Urun
+            //{
+            //    id = c.ToString()
+            //    if (id )
+            //}).ToList();
+
+            
+        }
+        public class HttpHelper
+        {
+            public static async Task<T> GetDataFromApi<T>(string url)
+            {
+                using (var client = new System.Net.Http.HttpClient())
+                {
+                    var result = await client.GetAsync(url);
+                    result.EnsureSuccessStatusCode();
+                    string resultContentString = await result.Content.ReadAsStringAsync();
+                    T resultContent = JsonConvert.DeserializeObject<T>(resultContentString);
+                    return resultContent;
+                }
+            }
+        }
+
+    }
+}
---
+++ Gorest1/Gorest1/Urun.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gorest1
+{
+    public class Urun
+    {
+         public string id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string status { get; set; }
+    }
+}
